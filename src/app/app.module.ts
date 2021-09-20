3cc6edcb4ebf4d8d62d8632ae9978ee489d7ec22
swethasri1707 <swethasri1707@gmail.com>
2021-09-20 17:18:45 +0530

    retailerhome1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RetailerHomeComponent } from './retailer-home/retailer-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RetailerHomeComponent
   ],
   imports: [
     BrowserModule,
