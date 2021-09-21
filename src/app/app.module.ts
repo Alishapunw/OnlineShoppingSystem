77035302711af7c44eee5c455c935cc1e00a93c7
sathvik-12 <sathvik.allani07@gmail.com>
2021-09-21 13:09:07 +0530

    firstcommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,31 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RegistrationComponent } from './registration/registration.component';
 import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { NavpageComponent } from './homepage/navpage/navpage.component';
+import { FooterpageComponent } from './homepage/footerpage/footerpage.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { FilterComponent } from './shopping/filter/filter.component';
+import { ProductlistComponent } from './shopping/productlist/productlist.component';
+import { CartComponent } from './shopping/cart/cart.component';
+import { CartitemComponent } from './shopping/cart/cartitem/cartitem.component';
+import { ProductitemComponent } from './shopping/productlist/productitem/productitem.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegistrationComponent,
-    ForgotpasswordComponent
+    ForgotpasswordComponent,
+    HomepageComponent,
+    NavpageComponent,
+    FooterpageComponent,
+    ShoppingComponent,
+    FilterComponent,
+    ProductlistComponent,
+    CartComponent,
+    CartitemComponent,
+    ProductitemComponent
   ],
   imports: [
     BrowserModule,
